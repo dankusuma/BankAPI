1afefa23f54b10e3301684786d12c622a3aa1792
dankusuma <kusumayudha32@gmail.com>
2021-12-08 05:00:18 +0800

    Add Unit Test

---
+++ UnitTest/Users_Test/User_HashPassword.cs
@@ -0,0 +1,25 @@
+﻿using Bank.Core.Entity;
+using NUnit.Framework;
+
+namespace UnitTest.Users_Test
+{
+    [TestFixture]
+    public class User_HashPassword
+    {
+        private User user;
+
+        [SetUp]
+        public void SetUp()
+        {
+            user = new User();
+            user.password = "admin";
+            
+        }
+        [Test]
+        public void HashPassword_ReturnTrue()
+        {
+            user.HashPassword();
+            Assert.AreEqual(user.password, "d033e22ae348aeb5660fc2140aec35850c4da997");
+        }
+    }
+}
---
+++ UnitTest/Users_Test/User_VerifyPassword.cs
@@ -0,0 +1,25 @@
+﻿using Bank.Core.Entity;
+using NUnit.Framework;
+
+namespace UnitTest.Users_Test
+{
+    [TestFixture]
+    public class User_VerifyPassword
+    {
+        private User user;
+
+        [SetUp]
+        public void SetUp()
+        {
+            user = new User();
+            user.password = "d033e22ae348aeb5660fc2140aec35850c4da997";
+        }
+
+        [Test]
+        public void VerifyPassword_ReturnTrue()
+        {
+            var result = user.VerifyPassword("admin");
+            Assert.IsTrue(result);
+        }
+    }
+}
