c6e253f6d6433d255755e55e767bdef91a20fad4
MITRAIS\Romario_P852 <Romario_P852@mitrais.com>
2022-01-14 08:20:51 +0700

    BA-106 Change Password & PIN - Add DOB validation.

--- Bank.Core/Entity/Pin.cs
+++ Bank.Core/Entity/Pin.cs
@@ -19,30 +19,25 @@ namespace Bank.Core.Entity
 
         public string PinValidation()
         {
-            //string yrs = user.BIRTH_DATE.Year.ToString();
-            //string mon = user.BIRTH_DATE.Month.ToString();
-            //string day = user.BIRTH_DATE.Day.ToString();
+            string yrs = user.BIRTH_DATE.Year.ToString().Substring(2, 2);
+            string mon = user.BIRTH_DATE.Month.ToString();
+            string day = user.BIRTH_DATE.Day.ToString();
+
+            string dob1 = day + mon + yrs;
+            string dob2 = yrs + mon + day;
 
             //if (user == null || user.USERNAME == "") return "Username not registered.";
 
-            if (mode == "create")
-            {
-                if (PIN == null) return "Invalid pin";
-                if (PIN == "") return "Invalid input. Only accept 6 digit numbers";
-                if (!PIN.All(char.IsNumber)) return "Only accept 6 digit numbers";
-                if (PIN.Length < 6) return "Pin too short. Only accept 6 digit numbers";
-                if (PIN.Length > 6) return "Pin too long. Only accept 6 digit numbers";
-                if (PIN is "123456" or "654321") return "Please DO NOT use \"123456\" or \"654321\" and the other general number as your pin";
-            }
+            if (PIN == null || NEW_PIN == null) return "Invalid pin";
+            if (PIN == "" || NEW_PIN == "") return "Invalid input. Only accept 6 digit numbers";
+            if (!PIN.All(char.IsNumber) || !NEW_PIN.All(char.IsNumber)) return "Only accept 6 digit numbers";
+            if (PIN.Length < 6 || NEW_PIN.Length < 6) return "Pin too short. Only accept 6 digit numbers";
+            if (PIN.Length > 6 || NEW_PIN.Length > 6) return "Pin too long. Only accept 6 digit numbers";
+            if (PIN == dob1 || PIN == dob2 || NEW_PIN == dob1 || NEW_PIN == dob2) return "Please do not use your Date of Birth (DOB)";
+            if (PIN is "123456" or "654321" || NEW_PIN is "123456" or "654321") return "Please DO NOT use \"123456\" or \"654321\" and the other general number as your pin";
 
             if (mode == "change")
             {
-                if (NEW_PIN == null) return "Invalid pin";
-                if (NEW_PIN == "") return "Invalid input. Only accept 6 digit numbers";
-                if (!NEW_PIN.All(char.IsNumber)) return "Only accept 6 digit numbers";
-                if (NEW_PIN.Length < 6) return "Pin too short. Only accept 6 digit numbers";
-                if (NEW_PIN.Length > 6) return "Pin too long. Only accept 6 digit numbers";
-                if (NEW_PIN is "123456" or "654321") return "Please DO NOT use \"123456\" or \"654321\" and the other general number as your pin";
                 if (NEW_PIN == PIN) return "New pin must be different from the old one.";
             }
 
