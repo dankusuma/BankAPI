f47968a0606a5ca045a2510cbc5ab1448a47a5c4
Saiful Setiawan <saiful.setiawan@mitrais.com>
2022-01-26 14:11:47 +0700

    create change address class

---
+++ Bank.Core/Entity/ChangeAddress.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Bank.Core.Entity
+{
+    public class ChangeAddress
+    {
+        public string TOKEN { get; set; }
+        public string USERNAME { get; set; }
+        public string ADDRESS { get; set; }
+        public string PROVINCE { get; set; }
+        public string KABUPATEN_KOTA { get; set; }
+        public string KECAMATAN { get; set; }
+        public string KELURAHAN { get; set; }
+    }
+}
