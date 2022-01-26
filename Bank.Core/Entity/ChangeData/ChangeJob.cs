aa8935b1e6b61aa52a872804f2f598c3d186326e
Saiful Setiawan <saiful.setiawan@mitrais.com>
2022-01-26 14:18:20 +0700

    create model for change job

---
+++ Bank.Core/Entity/ChangeData/ChangeJob.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Core.Entity.ChangeData
+{
+    public class ChangeJob
+    {
+        public string USERNAME { get; set; }
+        public string JOB { get; set; }
+    }
+}
