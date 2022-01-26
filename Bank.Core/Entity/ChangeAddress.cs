69cd93548d3d05cf1d74ce65eb9ac1fac4b32124
Saiful Setiawan <saiful.setiawan@mitrais.com>
2022-01-26 14:12:17 +0700

    create method for validator

--- Bank.Core/Entity/ChangeAddress.cs
+++ Bank.Core/Entity/ChangeAddress.cs
@@ -16,5 +16,31 @@ namespace Bank.Core.Entity
         public string KABUPATEN_KOTA { get; set; }
         public string KECAMATAN { get; set; }
         public string KELURAHAN { get; set; }
+
+        public bool IsAddressValid()
+        {
+            if (string.IsNullOrEmpty(ADDRESS) || ADDRESS.Length >= 200)
+            {
+                throw new ArgumentException("Alamat is too long");
+            }
+            if (string.IsNullOrEmpty(KELURAHAN) || !Regex.IsMatch(KELURAHAN, @"^[a-zA-Z ]+$") || KELURAHAN.Length > 50)
+            {
+                throw new ArgumentException("Kelurahan is not valid!");
+            }
+            if (string.IsNullOrEmpty(KABUPATEN_KOTA) || !Regex.IsMatch(KABUPATEN_KOTA, @"^[a-zA-Z ]+$") || KABUPATEN_KOTA.Length > 50)
+            {
+                throw new ArgumentException("Kabupaten is not valid!");
+            }
+            if (string.IsNullOrEmpty(KECAMATAN) || !Regex.IsMatch(KECAMATAN, @"^[a-zA-Z ]+$") || KECAMATAN.Length > 50)
+            {
+                throw new ArgumentException("Kecamatan is not valid!");
+            }
+            if (string.IsNullOrEmpty(PROVINCE) || !Regex.IsMatch(PROVINCE, @"^[a-zA-Z ]+$") || PROVINCE.Length > 50)
+            {
+                throw new ArgumentException("Province is not valid!");
+            }
+
+            return true;
+        }
     }
 }
