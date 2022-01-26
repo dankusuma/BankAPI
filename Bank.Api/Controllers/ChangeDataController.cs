2d85ef3009462dfd1aa6db93b3209e86942f088e
Saiful Setiawan <saiful.setiawan@mitrais.com>
2022-01-26 14:12:09 +0700

    create change data controller class

---
+++ Bank.Api/Controllers/ChangeDataController.cs
@@ -0,0 +1,25 @@
+﻿using Bank.Core.Entity;
+using Bank.Core.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Bank.Api.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class ChangeDataController : ControllerBase
+    {
+        private readonly IRepository _repository;
+        private readonly List<User> _users;
+
+        public ChangeDataController(IRepository rep)
+        {
+            _repository = rep;
+            _users = _repository.List<User>(null);
+        }
+    }
+}
